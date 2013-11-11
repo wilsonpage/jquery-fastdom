ca92056a564ca657c09036d8802fe648104ce85b
Wilson Page <wilsonpage@me.com>
2013-11-10 16:28:03 -0800

    Stuff

---
+++ index.js
@@ -0,0 +1,32 @@
+
+(function($){
+  var html = $.fn.html;
+
+  $.fn.html = function(str, done) {
+
+    // Accept callback as first
+    // or second argument
+    if (typeof str === 'function') {
+      done = str;
+      str = null;
+    }
+
+    // Callback is optional
+    done = done || function(){};
+
+    var ctx = this;
+    var type = str ? 'write' : 'read';
+
+    // If a string is not provided
+    // we don't want to call the
+    // original method with any args
+    var args = str ? [str] : [];
+
+    // Perform the original jquery
+    // method, but wrapped inside
+    // a fastdom callback
+    fastdom[type](function() {
+      done(html.apply(ctx, args));
+    });
+  };
+})(jQuery);
